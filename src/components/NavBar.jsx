80361499b93a9ec83d9af7392d8cae3dd42bf2ec
Iturri Santiago <santiagoiturrivargas04@gmail.com>
2024-07-31 17:38:35 -0400

    Create NavBar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,31 @@
+export const NavBar = () => {
+    const closeMenu = () => {
+        const hamburger = document.querySelector('.hamburger');
+        const navContent = document.querySelector('.nav-content');
+        const body = document.querySelector('body');
+
+        if (hamburger) hamburger.classList.remove('active');
+        if (navContent) navContent.classList.remove('active');
+        if (body) body.classList.remove('no-scroll');
+    };
+
+    return (
+        <>
+            <div>
+                <nav>
+                    <ul>
+                        <li><a onClick={() => closeMenu()} href='#home'>Home</a></li>
+                        <li><a onClick={() => closeMenu()} href='#projects'>Projects</a></li>
+                        <li><a onClick={() => closeMenu()} href='#skills'>Skills</a></li>
+                        <li><a onClick={() => closeMenu()} href='#about'>About me</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div className="social-links">
+                <a href="https://www.instagram.com/iturri_santiago/">Instagram</a>
+                <a href="https://github.com/Ichurri">GitHub</a>
+                <a href="https://www.linkedin.com/in/santiago-iturri-969003320/">LinkedIn</a>
+            </div>
+        </>
+    )
+}
